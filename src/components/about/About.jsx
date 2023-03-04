e04f441b3bee736f101eae369cfdc43ced7d9018
shishirkona <shoptadinga@gmail.com>
2023-03-05 01:37:11 +0600

    new update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+        <div className="about__cards">
+          <article className="about__card">
+            <FaAward className="about__icon"/>
+            <h5>Experience</h5>
+            <small>15+ Years Working</small>
+          </article>
+          <article className="about__card">
+            <FiUsers className="about__icon"/>
+            <h5>Clients</h5>
+            <small>100+ WorldWide</small>
+          </article>
+          <article className="about__card">
+            <VscFolderLibrary className="about__icon"/>
+            <h5>Projects</h5>
+            <small>500+ Completed</small>
+          </article>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mollis semper leo in congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus maximus eros ut eros eleifend, posuere rutrum ligula convallis.</p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+      </div>
+      </div>
+    </section>
   )
 }
 
